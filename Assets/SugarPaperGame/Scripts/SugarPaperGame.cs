2908fa009f07aad048a7c941021dacd2157bb9e8
hwaaaang <68269611+Hwang2442@users.noreply.github.com>
2024-10-05 20:50:09 +0900

    사용성 개선
    - 조건식 수정

--- Assets/SugarPaperGame/Scripts/SugarPaperGame.cs
+++ Assets/SugarPaperGame/Scripts/SugarPaperGame.cs
@@ -55,7 +55,7 @@ public class SugarPaperGame : MonoBehaviour
 			float x = Input.GetAxis("Mouse X");
 			float y = Input.GetAxis("Mouse Y");
 
-			if (Mathf.Abs(x) < 0.1f || Mathf.Abs(y) < 0.1f)
+			if (Mathf.Abs(x) < 0.1f && Mathf.Abs(y) < 0.1f)
 				return;
 
             var question = questions.Peek();
