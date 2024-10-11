25389dff964887a5451be17938a277c04b495d21
hwaaaang <68269611+Hwang2442@users.noreply.github.com>
2024-10-11 22:55:35 +0900

    Timer 스크립트 추가

---
+++ Assets/SugarPaperGame/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float time;
+    [SerializeField] private UnityEvent onEnd;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
