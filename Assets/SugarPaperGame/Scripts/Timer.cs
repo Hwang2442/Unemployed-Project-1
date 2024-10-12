d350d7d196f280c014a9d61474460b117f859ce3
hwaaaang <68269611+Hwang2442@users.noreply.github.com>
2024-10-12 23:36:30 +0900

    Timer 스크립트 초기화 로직 추가

--- Assets/SugarPaperGame/Scripts/Timer.cs
+++ Assets/SugarPaperGame/Scripts/Timer.cs
@@ -8,13 +8,21 @@ public class Timer : MonoBehaviour
     [SerializeField] private float time;
     [SerializeField] private UnityEvent onEnd;
 
+    private float t;
+
     private void Awake()
     {
-        
+        t = 0;
     }
 
     private void Update()
     {
-        
+        t += Time.deltaTime;
+
+        if (t >= time)
+        {
+            onEnd?.Invoke();
+            t = 0;
+        }
     }
 }
