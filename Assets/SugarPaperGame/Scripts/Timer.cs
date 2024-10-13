3349d6575f292f8a68b181ee844858111a0faee5
hwaaaang <68269611+Hwang2442@users.noreply.github.com>
2024-10-13 22:23:56 +0900

    Timer 추가

--- Assets/SugarPaperGame/Scripts/Timer.cs
+++ Assets/SugarPaperGame/Scripts/Timer.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
+using TMPro;
+using System;
 
 public class Timer : MonoBehaviour
 {
@@ -9,20 +12,38 @@ public class Timer : MonoBehaviour
     [SerializeField] private UnityEvent onEnd;
 
     private float t;
+    private Slider slider;
+    private TextMeshProUGUI text;
 
     private void Awake()
     {
+        slider = GetComponentInChildren<Slider>();
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        
         t = 0;
+        SetTime();
     }
 
     private void Update()
     {
         t += Time.deltaTime;
+        SetTime();
 
         if (t >= time)
         {
             onEnd?.Invoke();
             t = 0;
         }
+    }
+
+    private void SetTime()
+    {
+        float remain = time - t;
+        slider.SetValueWithoutNotify(Mathf.Clamp01(remain / time));
+
+        int s = Mathf.FloorToInt(remain);
+        float ms = remain - s;
+        string outputText = string.Format("{0:D2} : {1}", s, ms.ToString("N2").Substring(2));
+        text.SetText(outputText);
     }
 }
