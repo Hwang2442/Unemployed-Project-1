f8b45d856f5436987e49e91e467349d871146684
Chohyunm1n <chm220@naver.com>
2024-10-26 00:04:16 +0900

    - Fever 기능 수정

--- Assets/Mole/FeverMode.cs
+++ Assets/Mole/FeverMode.cs
@@ -6,26 +6,17 @@ using UnityEngine.UI;
 
 public class FeverMode : MonoBehaviour
 {
-
-    [SerializeField]
-    private Slider feverSlider;
-    [SerializeField]
-    private TextMeshProUGUI sliderText;
-    [SerializeField]
-    private float feverDuration = 2f;
-    [SerializeField]
-    private AudioSource feverSound;
-    [SerializeField]
-    private AudioClip feverclip;
-    [SerializeField]
-    private AudioClip moleHitclip;
-    [SerializeField]
-    private GameObject feverParticle;
+    [SerializeField] private Slider feverSlider;
+    [SerializeField] private TextMeshProUGUI sliderText;
+    [SerializeField] private float feverDuration = 2f;
+    [SerializeField] private AudioSource feverSound;
+    [SerializeField] private AudioClip feverclip;
+    [SerializeField] private AudioClip moleHitclip;
+    [SerializeField] private GameObject feverParticle;
 
     private float feverAmount = 0f;
-
-    public bool isFeverMode = false;
-
+    private bool isFeverMode = false;
+    private bool isSuperFeverMode = false;
 
     public void FeverSetting()
     {
@@ -37,29 +28,41 @@ public class FeverMode : MonoBehaviour
     {
         if (!isFeverMode && feverAmount > 0)
         {
-            feverAmount -= Time.deltaTime * 0.01f; 
+            feverAmount -= Time.deltaTime * 0.01f;
             feverSlider.value = feverAmount;
         }
     }
 
     public void UpdateFeverMode(bool moleHit, int hitScore, out int score)
     {
-        //TestValue
         float increaseAmount = 15f;
-
         int increaseScore = 0;
 
-        if (isFeverMode)
+        if (isSuperFeverMode) 
         {
             if (moleHit)
             {
-                increaseScore = hitScore * 2;
+                increaseScore = hitScore * 4; 
                 feverSound.clip = feverclip;
                 feverSound.Play();
             }
-
         }
-        else
+        else if (isFeverMode) 
+        {
+            if (moleHit)
+            {
+                feverAmount += Time.deltaTime * increaseAmount; 
+                increaseScore = hitScore * 2; 
+                feverSound.clip = feverclip;
+                feverSound.Play();
+
+                if (feverAmount >= 1f)
+                {
+                    ActivateSuperFeverMode();
+                }
+            }
+        }
+        else 
         {
             if (moleHit)
             {
@@ -74,27 +77,9 @@ public class FeverMode : MonoBehaviour
                 ActivateFeverMode();
             }
         }
+
         feverSlider.value = feverAmount;
         score = increaseScore;
-    }
-
-    private void ActivateFeverMode()
-    {
-        isFeverMode = true;
-        feverSlider.maxValue = 1f;
-        feverSlider.value = 1f;
-        feverParticle.SetActive(true);
-        //StartCoroutine(FeverModeCoroutine());
-        Debug.Log("Fever Mode!");
-    }
-
-    private void EndFeverMode()
-    {
-        isFeverMode = false;
-        feverAmount = 0;
-        feverSlider.value = feverAmount;
-        feverSlider.maxValue = 1f;
-        feverParticle.SetActive(false);
     }
 
 
@@ -104,25 +89,60 @@ public class FeverMode : MonoBehaviour
 
         while (timeElapsed < feverDuration)
         {
-            feverSlider.value = Mathf.Lerp(1f, 0f, timeElapsed / feverDuration);
-
+            // Super Fever transition check
+            if (!isSuperFeverMode && feverAmount >= 1f)
+            {
+                ActivateSuperFeverMode();
+            }
 
             timeElapsed += Time.deltaTime;
-
             yield return null;
         }
 
         feverSlider.value = 0f;
-
         EndFeverMode();
+    }
+
+    private void ActivateFeverMode()
+    {
+        isFeverMode = true;
+        feverAmount = 0f;
+        feverSlider.maxValue = 1f;
+        feverSlider.value = 0f;
+        feverParticle.SetActive(true);
+        StartCoroutine(FeverModeCoroutine());
+        Debug.Log("Fever!");
+    }
+
+    private void ActivateSuperFeverMode()
+    {
+        isSuperFeverMode = true;
+        isFeverMode = false;
+        sliderText.text = "S U P E R  F E V E R";
+        feverAmount = 0f;
+        feverSlider.value = 0f;
+        feverParticle.SetActive(true);
+        Debug.Log("Super Fever!");
+    }
+
+    private void EndFeverMode()
+    {
+        isFeverMode = false;
+        isSuperFeverMode = false;
+        feverAmount = 0;
+        feverSlider.value = feverAmount;
+        feverSlider.maxValue = 1f;
+        feverParticle.SetActive(false);
+        sliderText.text = "F E V E R";
     }
 
     public void ResetFeverMode()
     {
-
         isFeverMode = false;
+        isSuperFeverMode = false;
         feverAmount = 0f;
         feverSlider.value = 0f;
+        sliderText.text = "F E V E R";
         StopAllCoroutines();
     }
 }
